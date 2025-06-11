7e52705534dd107c948d32b0f14e66ce54944fcb
Nestive Web Studios <support@nestiveweb.com>
2025-06-11 11:14:31 -0400

    Update main.js
    Corrected errors in the main.js boot

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import "./style.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Make sure this matches your file name
-import './style.css'; // Optional but good to keep if you already have it
+import App from './App';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
