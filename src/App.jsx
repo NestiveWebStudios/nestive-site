25ef52ee8ba555d76f96a6e6bc36b60abfdf33de
Nestive Web Studios <support@nestiveweb.com>
2025-06-11 11:05:14 -0400

    Create App.jsx
    Created app.jsx for react routing

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Upgrade from './Upgrade';
+
+const App = () => {
+  return (
+    <Router>
+      <nav style={{ padding: '20px', backgroundColor: '#000' }}>
+        <Link to="/" style={{ color: 'white', marginRight: '20px' }}>Home</Link>
+        <Link to="/upgrade" style={{ color: 'white' }}>Upgrade</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upgrade" element={<Upgrade />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
