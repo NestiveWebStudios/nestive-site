eabfd90f08f193906991e080d1e3af205b3ae659
Nestive Web Studios <support@nestiveweb.com>
2025-06-11 10:57:07 -0400

    Update main.js
    Added React boot to main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Make sure this matches your file name
+import './style.css'; // Optional but good to keep if you already have it
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
